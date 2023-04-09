d766639d78892b4855e375bdd3f52eea2c8a5143
tournesol014s <86195166+tournesol014s@users.noreply.github.com>
2023-04-09 23:00:02 +0900

    Enable capacity provider setting

--- ecs_cluster.tf
+++ ecs_cluster.tf
@@ -7,6 +7,35 @@ resource "aws_ecs_cluster" "capacity_provider_test" {
   }
 }
 
+resource "aws_ecs_cluster_capacity_providers" "capacity_provider_test" {
+  cluster_name = aws_ecs_cluster.capacity_provider_test.name
+
+  capacity_providers = [aws_ecs_capacity_provider.capacity_provider_test.name]
+
+  default_capacity_provider_strategy {
+    base              = 1
+    weight            = 100
+    capacity_provider = aws_ecs_capacity_provider.capacity_provider_test.name
+  }
+}
+
+resource "aws_ecs_capacity_provider" "capacity_provider_test" {
+  name = "capacity-provider-test"
+
+  auto_scaling_group_provider {
+    auto_scaling_group_arn         = aws_autoscaling_group.capacity_provider_test.arn
+    managed_termination_protection = "ENABLED"
+
+    managed_scaling {
+      instance_warmup_period    = 60
+      maximum_scaling_step_size = 3
+      minimum_scaling_step_size = 1
+      status                    = "ENABLED"
+      target_capacity           = 90
+    }
+  }
+}
+
 resource "aws_launch_template" "capacity_provider_test" {
   name          = "capacity-provider-test"
   image_id      = data.aws_ssm_parameter.ecs_optimized_ami_image_id.value
@@ -28,12 +57,12 @@ resource "aws_launch_template" "capacity_provider_test" {
 }
 
 resource "aws_autoscaling_group" "capacity_provider_test" {
-  name                = "capacity-provider-test"
-  vpc_zone_identifier = [aws_subnet.private_1a[0].id, aws_subnet.private_1c[0].id, aws_subnet.private_1d[0].id]
-  desired_capacity    = 1
-  max_size            = 3
-  min_size            = 1
-
+  name                  = "capacity-provider-test"
+  vpc_zone_identifier   = [aws_subnet.private_1a[0].id, aws_subnet.private_1c[0].id, aws_subnet.private_1d[0].id]
+  desired_capacity      = 1
+  max_size              = 3
+  min_size              = 1
+  protect_from_scale_in = true
   mixed_instances_policy {
     launch_template {
       launch_template_specification {
@@ -54,54 +83,14 @@ resource "aws_autoscaling_group" "capacity_provider_test" {
   lifecycle {
     ignore_changes = [desired_capacity]
   }
+
+  tag {
+    key                 = "AmazonECSManaged"
+    value               = true
+    propagate_at_launch = true
+  }
 }
 
 data "aws_ssm_parameter" "ecs_optimized_ami_image_id" {
   name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
-}
-
-resource "aws_autoscaling_policy" "capacity_provider_test_scale_out_cpu" {
-  name                   = "capacity-provider-test-scale-out-cpu"
-  scaling_adjustment     = 1
-  adjustment_type        = "ChangeInCapacity"
-  cooldown               = 60
-  autoscaling_group_name = aws_autoscaling_group.capacity_provider_test.name
-}
-
-resource "aws_autoscaling_policy" "capacity_provider_test_scale_in_cpu" {
-  name                   = "capacity-provider-test-scale-in-cpu"
-  scaling_adjustment     = -1
-  adjustment_type        = "ChangeInCapacity"
-  cooldown               = 60
-  autoscaling_group_name = aws_autoscaling_group.capacity_provider_test.name
-}
-
-resource "aws_cloudwatch_metric_alarm" "capacity_provider_test_cpu_high" {
-  alarm_name          = "capacity-provider-test-cpu-high"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "1"
-  metric_name         = "CPUReservation"
-  namespace           = "AWS/ECS"
-  period              = "60"
-  statistic           = "Average"
-  threshold           = "70"
-  dimensions = {
-    ClusterName = aws_ecs_cluster.capacity_provider_test.name
-  }
-  alarm_actions = ["${aws_autoscaling_policy.capacity_provider_test_scale_out_cpu.arn}"]
-}
-
-resource "aws_cloudwatch_metric_alarm" "capacity-provider-test-cpu-low" {
-  alarm_name          = "capacity-provider-test-cpu-low"
-  comparison_operator = "LessThanThreshold"
-  evaluation_periods  = "1"
-  metric_name         = "CPUReservation"
-  namespace           = "AWS/ECS"
-  period              = "60"
-  statistic           = "Average"
-  threshold           = "30"
-  dimensions = {
-    ClusterName = aws_ecs_cluster.capacity_provider_test.name
-  }
-  alarm_actions = ["${aws_autoscaling_policy.capacity_provider_test_scale_in_cpu.arn}"]
 }
